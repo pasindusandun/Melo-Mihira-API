275c10a729006730575cdbe9aacaed5585e5bad9
pasindu <it19390260@my.sliit.lk>
2023-08-13 13:23:53 +0530

    update

--- API_Implementation.py
+++ API_Implementation.py
@@ -5,7 +5,7 @@ import tensorflow
 from fastapi import *
 import os
 
-import ModelLoading
+# import ModelLoading
 # import activity
 # import emotionModel
 # import music_recommendation
@@ -24,27 +24,27 @@ def GenderMapper(gender_):
         return genders[0]
 
 
-# @app.get('/HelloWorld')
-# def hello_world():
-#     return "Hello World"
+@app.get('/HelloWorld')
+def hello_world():
+    return "Hello World"
 
 
-@app.post("/demographicsImage")
-async def create_upload_file(file: UploadFile = File(...)):
-    file.filename = f"{uuid.uuid4()}.jpg"
-    contents = await file.read()  # <-- Important!
+# @app.post("/demographicsImage")
+# async def create_upload_file(file: UploadFile = File(...)):
+#     file.filename = f"{uuid.uuid4()}.jpg"
+#     contents = await file.read()  # <-- Important!
 
-    # example of how you can save the file
-    with open(f"{IMAGEDIR}{file.filename}", "wb") as f:
-        f.write(contents)
+#     # example of how you can save the file
+#     with open(f"{IMAGEDIR}{file.filename}", "wb") as f:
+#         f.write(contents)
 
-    filepath = IMAGEDIR + file.filename
-    Result = ModelLoading.finalImageOutput(filepath)
-    # Recommendations = music_recommendation.recommendDemographics(Result[0], GenderMapper(Result[1]), Result[2])
+#     filepath = IMAGEDIR + file.filename
+#     Result = ModelLoading.finalImageOutput(filepath)
+#     # Recommendations = music_recommendation.recommendDemographics(Result[0], GenderMapper(Result[1]), Result[2])
 
-    return {"Gender": Result[1], "Age": Result[0], "Ethnicity": Result[2]
-            # , "Recommendations": Recommendations
-            }
+#     return {"Gender": Result[1], "Age": Result[0], "Ethnicity": Result[2]
+#             # , "Recommendations": Recommendations
+#             }
 
 
 # @app.post("/emotionImage")
